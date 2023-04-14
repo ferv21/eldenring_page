8e645e01d829a4edcdf09ecbef8c5730749a9898
Fernando Viana <fernandovianadossantos2001@gmail.com>
2023-04-14 16:53:53 -0400

    finalizando a landing page

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-
+const imagemin = require('gulp-imagemin');
 
 
 function styles(){
@@ -11,8 +11,15 @@ function styles(){
     .pipe(gulp.dest('./dist/css'));
 }
 
+function images(){
+    return gulp.src('./src/images/**')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
+}
 
-exports.default = gulp.parallel(styles);
+
+
+exports.default = gulp.parallel(styles,images);
 exports.watch = function(){
     gulp.watch('./src/styles/*scss', gulp.parallel(styles));
 }
